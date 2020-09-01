39a687c1b617de63f5b065472a6fe5b151f22dc8
wly86 <451215954@qq.com>
2020-09-01 14:27:00 +0800

    绘制旋转地球

--- DemoSunAndEarthWithGLSL/DemoSunAndEarthWithGLSL/main.cpp
+++ DemoSunAndEarthWithGLSL/DemoSunAndEarthWithGLSL/main.cpp
@@ -5,6 +5,32 @@
 int steps = 30;
 const float angle = 3.1415926 * 2 / steps;
 
+void DrawCircle(float red, float green, float blue) {
+	float xPos = 0;
+	float yPos = 0;
+	float radius = 1.0f;
+	float prevX = xPos;
+	float prevY = radius - yPos;
+
+	for (int i = 0; i <= steps; i++)
+	{
+		float newX = radius * sin(angle * i);
+		float newY = radius * cos(angle * i);
+
+		glBegin(GL_TRIANGLES);
+
+		glColor3f(red, green, blue);
+		glVertex3f(-0.0f, 0.0f, 0.f);
+		glVertex3f(prevX, prevY, 0.f);
+		glVertex3f(newX, newY, 0.f);
+
+		glEnd();
+
+		prevX = newX;
+		prevY = newY;
+	}
+}
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -14,7 +40,7 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(800, 800, "Hello World", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -24,51 +50,36 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
-	float xPos = 0;
-	float yPos = 0;
-	float radius = 1.0f;
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glScalef(0.1, 0.1, 1);    //[-1,1] -->[-10,10]  ÆÁÄ»×ø±ê·¶Î§
+
+	float angle = 0;
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
+		angle += 1;
+
 		glClearColor(1.0, 1.0, 1.0, 0);
 
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		//! Í¼Ôª»æÖÆ
-		/*glBegin(GL_TRIANGLES);
-
-		glColor3f(1.0, 0, 0);
-		glVertex3f(-0.6f, -0.4f, 0.f);
-		glColor3f(0.0, 1.0, 0);
-		glVertex3f(0.6f, -0.4f, 0.f);
-		glColor3f(0.0, 0, 1.0);
-		glVertex3f(0.f, 0.6f, 0.f);
-
-		glEnd();*/
-
-		float prevX = xPos;
-		float prevY = radius - yPos;
-
-		for (int i = 0; i <= steps; i++)
+		//!»æÖÆÌ«Ñôsun
+		DrawCircle(1.0, 1.0, 0);
+		
+		//! µØÇò
 		{
-			float newX = radius * sin(angle * i);
-			float newY = radius * cos(angle * i);
-
-			glBegin(GL_TRIANGLES);
-
-			glColor3f(1.0, 0.5, 0);
-			glVertex3f(-0.0f, 0.0f, 0.f);
-			glVertex3f(prevX, prevY, 0.f);
-			glVertex3f(newX, newY, 0.f);
-
-			glEnd();
-
-			prevX = newX;
-			prevY = newY;
+			glPushMatrix();
+			
+			glRotatef(angle, 0, 0, 1);   //ÈÆzÖáÐý×ª
+			glTranslatef(0, 5, 0);
+			glScalef(0.6, 0.6, 0.6);
+			DrawCircle(0, 0.3, 1);
+			glPopMatrix();
 		}
-
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
