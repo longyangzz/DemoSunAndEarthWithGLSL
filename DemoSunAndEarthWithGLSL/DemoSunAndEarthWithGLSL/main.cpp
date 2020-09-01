714ac02082c149f67d1ad77c483b315829762cfc
wly86 <451215954@qq.com>
2020-09-01 16:12:17 +0800

    glew 扩展使用glsl绘制三角形完成，黑窗口，渲染没成功

--- DemoSunAndEarthWithGLSL/DemoSunAndEarthWithGLSL/main.cpp
+++ DemoSunAndEarthWithGLSL/DemoSunAndEarthWithGLSL/main.cpp
@@ -1,9 +1,10 @@
+#include "gl/glew.h"
 #include <GLFW/glfw3.h>
-
 #include "math.h"
 
-int steps = 30;
-const float angle = 3.1415926 * 2 / steps;
+#include <windows.h>
+
+#include <iostream>
 
 void DrawCircle(float red, float green, float blue) {
 	float xPos = 0;
@@ -11,7 +12,8 @@ void DrawCircle(float red, float green, float blue) {
 	float radius = 1.0f;
 	float prevX = xPos;
 	float prevY = radius - yPos;
-
+	const int steps = 10;
+	const float angle = 3.1415936 * 2 / 360;
 	for (int i = 0; i <= steps; i++)
 	{
 		float newX = radius * sin(angle * i);
@@ -31,9 +33,52 @@ void DrawCircle(float red, float green, float blue) {
 	}
 }
 
-int main(void)
+const GLfloat vertices[] = {
+	-1.0f, -1.0f, 0.0f,
+	 1.0f, -1.0f, 0.0f,
+	 1.0f,  1.0f, 0.0f,
+	-1.0f, -1.0f, 0.0f,
+	 1.0f,  1.0f, 0.0f,
+	-1.0f,  1.0f, 0.0f
+};
+
+const GLfloat colors[] = {
+	0.0f, 0.0f, 1.0f,
+	0.0f, 1.0f,0.0f,
+	1.0f, 0.0f, 0.0f,
+	0.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 0.0f,
+	0.0f, 1.0f, 0.0f
+};
+
+const GLchar* vertex120 = R"END(
+#version 120
+attribute vec4 inColor;
+attribute vec4 inPosition;
+uniform mat4 matrix;
+varying vec4 outColor;
+void main()
+{
+	outColor = inColor;
+	gl_Position = inPosition * matrix;
+}
+)END";
+
+const GLchar* raster120 = R"END(
+#version 120
+varying vec4 outColor;
+void main()
+{
+	gl_FragColor = outColor;
+}
+)END";
+
+int main(int argc, char *argv[])
 {
 	GLFWwindow* window;
+	//初始化freeglut
+	//glutInit(&argc, argv);
+	
 
 	/* Initialize the library */
 	if (!glfwInit())
@@ -50,48 +95,115 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	glScalef(0.1, 0.1, 1);    //[-1,1] -->[-10,10]  屏幕坐标范围
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to init GLEW" << std::endl;
+		return -1;
+	}
 
-	float angle = 0;
-	float angleMoon = 0;
+
+	// get version info
+	const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+	const GLubyte* version = glGetString(GL_VERSION); // version as a string
+	printf("Renderer: %s\n", renderer);
+	printf("OpenGL version supported %s\n", version);
+
+	//!1、 顶点着色器
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertex120, 0);
+	glCompileShader(vertexShader);
+
+	GLint compilationStatus;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &compilationStatus);
+	if(compilationStatus == GL_FALSE){
+		GLchar message[256];
+		glGetShaderInfoLog(vertexShader, sizeof(message),0,&message[0]);
+		std::cout << message;
+		exit(1);
+	}
+
+	//!2、 片元着色器
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &raster120, 0);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &compilationStatus);
+	if (compilationStatus == GL_FALSE) {
+		GLchar message[256];
+		glGetShaderInfoLog(fragmentShader, sizeof(message), 0, &message[0]);
+		std::cout << message;
+		exit(1);
+	}
+
+	//！3、 shader program
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &compilationStatus);
+	if (compilationStatus == GL_FALSE) {
+		GLchar message[256];
+		glGetShaderInfoLog(shaderProgram, sizeof(message), 0, &message[0]);
+		std::cout << message;
+		exit(1);
+	}
+
+	glUseProgram(shaderProgram);
+
+	//！4、vbo setup
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+	GLuint colorsBuffer;
+	glGenBuffers(1, &colorsBuffer);
+
+	//!5、send data to gpu
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, colorsBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colorsBuffer), colors, GL_STATIC_DRAW);
+
+	//5、locattion
+	GLint attribPosition = glGetAttribLocation(shaderProgram, "inPosition");
+	glEnableVertexAttribArray(attribPosition);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glVertexAttribPointer(attribPosition, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	GLint attribColor = glGetAttribLocation(shaderProgram, "inColor");
+	glEnableVertexAttribArray(attribColor);
+	glBindBuffer(GL_ARRAY_BUFFER, colorsBuffer);
+	glVertexAttribPointer(attribColor, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	//!6\ uniforms
+	GLuint attributeMatrix = glGetUniformLocation(shaderProgram, "matrix");
+
+	float alpha = 0;
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
-		angle += 1;
-
 		glClearColor(0.0, 0.0, 0.0, 0);
 
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		//! 图元绘制
-		//!绘制太阳sun
-		DrawCircle(1.0, 1.0, 0);
-		
-		//! 地球
-		{
-			glPushMatrix();
-			
-			glRotatef(angle, 0, 0, 1);   //绕z轴旋转
-			glTranslatef(0, 5, 0);
-			glScalef(0.6, 0.6, 0.6);
-			DrawCircle(0, 0.3, 1);
+		float sa = 0.5 * sin(alpha);
+		float ca = 0.5 * cos(alpha);
+		alpha += 0.1;
 
-			//! 绘制月亮
-			{
-				glPushMatrix();
-				glRotatef(angleMoon, 0, 0, 1);
-				glTranslatef(0, 3, 0);
-				glScalef(0.5, 0.5, 1);
-				DrawCircle(0.5, 0.5, 0.5);
-				glPopMatrix();
-				angleMoon += 3;
-			}
+		const GLfloat matrix[] = {
+			sa, -ca, 0, 0,
+			ca,  sa, 0, 0,
+			0,    0, 1, 0,
+			0,    0, 0, 1
+		};
+		glUniformMatrix4fv(attributeMatrix, 1, GL_FALSE, matrix);
 
-			glPopMatrix();
-		}
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
